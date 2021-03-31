4893cdacd1dcebeb25ba7d9803eee8d4678b8b62
NellyZhou <1253336185@qq.com>
2021-03-31 17:54:23 +0800

    DEBUG initial

--- Code/main.c
+++ Code/main.c
@@ -6,13 +6,13 @@
 TreeNode *root = NULL;
 int lexical_error = 0;
 int syntax_error = 0;
+int 
 
 extern FILE* yyin;
 #ifdef __YY_DEBUG__
 extern int yydebug;
 #endif
 extern int yylex (void);
-
 extern int yyparse(void);
 extern void yyrestart(FILE *);
 
